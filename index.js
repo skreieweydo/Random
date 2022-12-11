ae41e4f4bcd6237411ae596d88d24e0ad092b6f4
Michael James <micrjames921@gmail.com>
2022-12-10 18:26:59 -0700

    updated file that shows operations of Random class.

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const { Random } = require("./Random");
 
 const random = new Random(0, 9);
 
-const randNum = random.number;
+let randNum = random.number;
 console.log(`A random number from ${random.minimum} to ${random.maximum}: ${randNum}.`);
 
-const randInt = random.integer;
+let randInt = random.integer;
 console.log(`A random integer from ${random.minimum} to ${random.maximum}: ${randInt}.`);
 
 const zeroOrOne = random.zeroOrOne;
@@ -15,6 +15,17 @@ let nums = [2, 4];
 const randChoice = random.choice(nums);
 
 console.log(`A random element of ${nums}: ${randChoice}.`);
+console.log(`minimum: ${random.minimum} and maximum: ${random.maximum}`);
 
 nums = [2, 4, 6];
 console.log(`A random element of ${nums}: ${randChoice}.`);
+console.log(`minimum: ${random.minimum} and maximum: ${random.maximum}`);
+
+random.minimum = 0;
+random.maximum = 9;
+
+randNum = random.number;
+console.log(`A random number from ${random.minimum} to ${random.maximum}: ${randNum}.`);
+
+randInt = random.integer;
+console.log(`A random integer from ${random.minimum} to ${random.maximum}: ${randInt}.`);
