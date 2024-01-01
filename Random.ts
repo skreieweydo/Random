fdaa2d2ccdbdb1a5ae7a43677f5657f739e9ea9c
Michael James <micrjames921@gmail.com>
2024-01-01 13:59:51 -0700

    rewrote 'Random.js' to 'Random.ts'; setup framework for code tests

---
+++ Random.ts
@@ -0,0 +1,41 @@
+class Random {
+    // "number" and "integer" functions returns are inclusive of max and min
+    private minimum: number;
+    private maximum: number;
+
+    constructor(minimum:number = -1, maximum: number = -1) {
+	    this.minimum = minimum;
+	    this.maximum = maximum;
+	}
+
+    set min(value: number) {
+	    this.minimum = value;
+	}
+    get min(): number{
+	    return this.minimum;
+	}
+    set max(value: number) {
+	    this.maximum = value;
+	}
+    get max(): number {
+	    return this.maximum;
+	}
+
+    get number(): number {
+		return Math.random() * (this.maximum - this.minimum + 1) + this.minimum;
+	}
+    get integer(): number {
+		return Math.floor(this.number);
+	}
+	get zeroOrOne(): number {
+	    return Math.round(Math.random());
+	}
+    choice(nums: number[]): number {
+	    this.minimum = 0;
+	    this.maximum = nums.length - 1;
+		
+	    return nums[this.integer];
+	}
+}
+
+exports.Random = Random;
